db05a3cab9f41275a2d2b2c57f8210219dda7971
nathan-08 <nathanryan001@gmail.com>
2018-03-22 15:53:57 -0600

    fixed bug on server

--- server/middleware/checkForSession.js
+++ server/middleware/checkForSession.js
@@ -1,5 +1,6 @@
 module.exports = function(req, res, next) {
   if (!req.session.user) {
+    console.log("creating new session");
     req.session.user = {
       username: "",
       loggedIn: false,
--- server/models/users.js
+++ server/models/users.js
@@ -1,1 +1,15 @@
-module.exports = [{ id: 999, username: "nate", password: "password" }];
+module.exports = [
+  {
+    id: 999,
+    username: "nate",
+    password: "password",
+    preferences: {
+      bgc: "cornflowerBlue",
+      fontColor: "pink",
+      favoriteThing: "bird",
+      title: "welcome to awesome page",
+      message: "nerner",
+      fontIndex: 0
+    }
+  }
+];
